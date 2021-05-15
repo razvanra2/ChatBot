927422011a5e354d93ced2e4d84bdabfe54a40e4
Razvan Radoi <radoi@adobe.com>
2021-05-15 18:04:38 +0300

    add model for restaurants

--- actions/actions.py
+++ actions/actions.py
@@ -12,16 +12,32 @@ import datetime as dt
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class RestaurantAPI:
+    def search(self, info):
+        return "papi's pizza place"
 
 
-class ActionShowTime(Action):
+class ActionSearchRestaurants(Action):
+    def name(self):
+        return "action_search_restaurants"
 
-    def name(self) -> Text:
-        return "action_show_time"
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="looking for restaurants")
+        restaurant_api = RestaurantAPI()
+        restaurants = restaurant_api.search(tracker.get_slot("cuisine"))
+        return [SlotSet("matches", restaurants)]
 
-    def run(self, dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text=f"{dt.datetime.now()}")
+class ActionSuggest(Action):
+    def name(self):
+        return "action_suggest"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="here's what I found:")
+        dispatcher.utter_message(text=tracker.get_slot("matches"))
+        dispatcher.utter_message(
+            text="is it ok for you? hint: I'm not going to find anything else :)"
+        )
         return []
