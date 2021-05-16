8665faa483a7b07473c9e3eb5fbde5763617893e
BogdanRadulescu <bogdan.radulescu97@gmail.com>
2021-05-16 13:11:43 +0300

    Added filtering for restaurants, restaurant db, dialogue logic

--- actions/actions.py
+++ actions/actions.py
@@ -13,31 +13,49 @@ import datetime as dt
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+import json
+
+with open("dataset/restaurant_db.json", "r") as read_file:
+    restaurants = json.load(read_file)
+
 
 class RestaurantAPI:
-    def search(self, info):
-        return "papi's pizza place"
+    def search(self, tracker: Tracker):
+        food = tracker.get_slot("cuisine")
+
+        area = tracker.get_slot("location")
+        all_zones = ["centre", "north", "south", "east", "west"]
+        area = all_zones if area not in all_zones else [area]
+
+        pricerange = tracker.get_slot("price")
+        pricerange = "cheap" if pricerange == "lo" else "expensive" if pricerange == "hi" else "moderate" if pricerange == "mid" else pricerange
+
+        print(food, area, pricerange)
+        options = [x for x in filter(lambda x: x["food"] == food and x["area"] in area and x["pricerange"] == pricerange, restaurants)]
+
+        return options
 
 
 class ActionSearchRestaurants(Action):
+
     def name(self):
         return "action_search_restaurants"
 
     def run(self, dispatcher, tracker, domain):
         dispatcher.utter_message(text="looking for restaurants")
         restaurant_api = RestaurantAPI()
-        restaurants = restaurant_api.search(tracker.get_slot("cuisine"))
-        return [SlotSet("matches", restaurants)]
+        res = restaurant_api.search(tracker)
+        return [SlotSet("matches", res)]
 
 
 class ActionSuggest(Action):
     def name(self):
         return "action_suggest"
 
-    def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message(text="here's what I found:")
-        dispatcher.utter_message(text=tracker.get_slot("matches"))
-        dispatcher.utter_message(
-            text="is it ok for you? hint: I'm not going to find anything else :)"
-        )
+    def run(self, dispatcher: CollectingDispatcher, tracker, domain):
+        res = tracker.get_slot("matches")
+
+        buttons = [{"title": x["name"], "payload": '/affirm{"name":"' + x["name"] + '"}'} for x in res] + [{"title": "No option", "payload": "/goodbye"}]
+        dispatcher.utter_message(text="here's what I found:", buttons=buttons)
+
         return []
